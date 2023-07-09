86ed86d62b0e46d0a1726e10a004e9868a56c05b
Dial <Dialsyphax@gmail.com>
2023-07-09 12:19:24 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./Pages/Accueil/App";
+import Error from "./Pages/Error";
+import URN from "./Pages/URN";
+import { gsap } from "gsap";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { ScrollTrigger, ScrollSmoother } from "gsap/all";
+import { MotionPathPlugin } from "gsap/dist/MotionPathPlugin";
+import "./Utils/Styles/Mouse.css";
+
+const Circle = forwardRef(({ size, delay }, ref) => {
+  const el = useRef();
+
+  useImperativeHandle(
+    ref,
+    () => {
+      // return our API
+      return {
+        moveTo(x, y) {
+          gsap.to(el.current, { x, y, delay });
+        },
+      };
+    },
+    [delay]
+  );
+
+  return <div className={`circle ${size}`} ref={el}></div>;
+});
+
+function Mouse() {
+  const circleRefs = useRef([]);
+
+  // reset on re-renders
+  circleRefs.current = [];
+
+  useEffect(() => {
+    const onMove = ({ clientX, clientY }) => {
+      circleRefs.current.forEach((ref) => ref.moveTo(clientX, clientY - 35));
+    };
+
+    window.addEventListener("pointermove", onMove);
+
+    return () => window.removeEventListener("pointermove", onMove);
+  }, []);
+
+  const addCircleRef = (ref) => {
+    if (ref) {
+      circleRefs.current.push(ref);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <Circle size="lg" ref={addCircleRef} delay={0.2} />
+      <Circle size="md" ref={addCircleRef} delay={0.1} />
+      <Circle size="sm" ref={addCircleRef} delay={0} />
+    </React.Fragment>
+  );
+}
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Mouse />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="URN" element={<URN />} />
+        <Route path="EPSI" />
+        <Route path="UMMTO" />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
